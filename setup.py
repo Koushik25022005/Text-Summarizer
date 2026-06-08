90cbacdf9a99004c15b61ccc02fd647445b6dd4a
Koushik Sripathi <sripathikoushik244@gmail.com>
2026-06-08 09:52:06 +0400

    Data ingestion started

--- setup.py
+++ setup.py
@@ -7,7 +7,23 @@ with open("README.md", "r", encoding="utf-8") as f:
     
 __version__= "0.0.1"
 
-REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "Koushik Sripathi"
+REPO_NAME = "Text-Summarizer"
+AUTHOR_USER_NAME = "Koushik25022005"
 AUTHOR_EMAIL = "sripathikoushik244@gmail.com"
 SRC_REPO = "textSummarizer"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A text summarization package using Hugging Face Transformers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
---
+++ src/TextSummarizer/utils/common.py
@@ -0,0 +1,48 @@
+import os 
+from box.exceptions import BoxValueError
+import yaml
+from TextSummarizer.logging import logger
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """read a yaml file and return as ConfigBox 
+
+    Args:
+        path_to_yaml (Path): _description_
+
+    Returns:
+        ConfigBox: _description_
+    """
+    
+    try:
+        with open(path_to_yaml) as y_file:
+            content = yaml.safe_load(y_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded succesfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError(f"yaml file: {path_to_yaml} is empty")
+    except Exception as e:
+        logger.error(f"Error occurred while loading yaml file: {path_to_yaml}")
+        raise e
+    
+
+
+def create_directories(path_to_directories: list, verbose=True):
+    """create a list of directories
+    Arguments:
+    path_to_directories: list of path of directories
+    verbose: bool, if True, print information about created directories
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created a directory at: {path}")
+            
+            
+            
+def get_size(path: Path) -> str:
+    """get size in kb"""
+    size_in_kb = round(os.path.getsize(path)/1024, 2)
+    return f"{size_in_kb} KB"
