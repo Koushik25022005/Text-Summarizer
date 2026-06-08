9f6699183baa611366fc41565ed3b8bffa9e8de3
Koushik Sripathi <sripathikoushik244@gmail.com>
2026-06-08 08:20:22 +0400

    Updating setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+    
+    
+__version__= "0.0.1"
+
+REPO_NAME = "Text-Summarizer-Project"
+AUTHOR_USER_NAME = "Koushik Sripathi"
+AUTHOR_EMAIL = "sripathikoushik244@gmail.com"
+SRC_REPO = "textSummarizer"
